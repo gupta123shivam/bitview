74c4575dff08f96682008ac97705a79da0e73713
Shivam <guptashivam9893@gmail.com>
2024-03-04 10:00:46 +0530

    set up react router and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { Navbar } from './components'
 import './styles/styles.scss'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Coin from './pages/Coin'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/coin' element={<Coin />}>
+          <Route path=':coinId' element={<Coin />} />
+        </Route>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {} from '../components'
+
+const Home = () => {
+  return <div>Home</div>
+}
+
+export default Home
